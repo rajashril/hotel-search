f4d900e08e27ae8bce34e1fc56cead61a3719f78
Rajashri Lawande <rajashrilawande@gmail.com>
2018-12-18 17:49:49 +0530

    added new

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+
+import { HotelService } from '../services/hotel.service';
+
+const now = new Date();
 
 @Component({
   selector: 'app-search-result',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchResultComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+  checkInDate: string;
+  checkOutDate: string;
+  numberOfGuests: Number;
+  minDate: NgbDateStruct = {year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate()};
+  constructor(private _hotelService: HotelService) { }
 
   ngOnInit() {
   }
 
+  onSelectResPage(): void{
+    this.checkInDate = this.model.checkIn.month+"/"+ this.model.checkIn.day+"/"+ this.model.checkIn.year;
+    this.checkOutDate = this.model.checkOut.month+"/"+ this.model.checkOut.day+"/"+ this.model.checkOut.year;
+    this.numberOfGuests = this.model.guest;
+    this._hotelService.someMethod(this.checkInDate, this.checkOutDate, this.numberOfGuests);
+  }
 }
