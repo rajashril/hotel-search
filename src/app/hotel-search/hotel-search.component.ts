69f4bd119da89f66be709ac081fc5e2d01317164
Rajashri Lawande <rajashrilawande@gmail.com>
2018-12-18 17:50:36 +0530

    added new

--- src/app/hotel-search/hotel-search.component.ts
+++ src/app/hotel-search/hotel-search.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router'; 
 
 import { HotelService } from '../services/hotel.service';
+
+const now = new Date();
 
 @Component({
   selector: 'app-hotel-search',
@@ -9,22 +13,23 @@ import { HotelService } from '../services/hotel.service';
 })
 export class HotelSearchComponent implements OnInit {
 
+  model: any = {};
+  checkInDate: string;
+  checkOutDate: string;
+  numberOfGuests: Number;
 
-    checkInDate: string;
-    checkOutDate: string;
-    numberOfGuests: Number;
+  minDate: NgbDateStruct = {year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate()};
 
-  constructor(private _hotelService: HotelService) { }
+  constructor(private _hotelService: HotelService, private router: Router) { }
 
   ngOnInit() {
-
   }
 
-  onSelect(checkInVal, checkOutVal, guestNum): void {
-    this.checkInDate = checkInVal.month+"/"+ checkInVal.day+"/"+ checkInVal.year;
-    this.checkOutDate = checkOutVal.month+"/"+ checkOutVal.day+"/"+ checkOutVal.year;
-    this.numberOfGuests = guestNum;
+  onSelect(): void {
+    this.checkInDate = this.model.checkIn.month+"/"+ this.model.checkIn.day+"/"+ this.model.checkIn.year;
+    this.checkOutDate = this.model.checkOut.month+"/"+ this.model.checkOut.day+"/"+ this.model.checkOut.year;
+    this.numberOfGuests = this.model.guest;
     this._hotelService.someMethod(this.checkInDate, this.checkOutDate, this.numberOfGuests);
+    this.router.navigate(['/result']);
   }
-
 }
